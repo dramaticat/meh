4bbd9877fbb523e5121b9be93861b521999b0076
Amanda Plotnik <geckos.claw@gmail.com>
2017-08-12 15:26:51 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var matches = 0
 
-function yesFunction(){
-matches=matches+1
-}
+    function buttonPress()
+    {
+          document.getElementById("theImage").src="http://31.media.tumblr.com/fca646cd8fe87906e605ad7e8d039903/tumblr_mmoz4fWT6U1soh1p8o1_500.png";
+    }
