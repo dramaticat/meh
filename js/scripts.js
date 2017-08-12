6829dcb08a3f207f0c6972ca2052ed8689b52469
Amanda Plotnik <geckos.claw@gmail.com>
2017-08-12 18:12:47 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-
+<script>
     function buttonPress()
     {
         $('http://webneel.com/daily/sites/default/files/images/daily/03-2016/5-corporate-business-card-design.preview.jpg').fadeOut('fast');
         
     }
-
+</script>
